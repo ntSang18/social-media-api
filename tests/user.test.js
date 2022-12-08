c32dbd44dff0530746384f01dd45d65b83912660
TienLort <vantien281201@gmail.com>
2022-12-08 21:56:29 +0700

    add user

---
+++ tests/user.test.js
@@ -0,0 +1,21 @@
+const request = require("supertest");
+const app = require("../app");
+const setupTestDB = require("./utils/setupTestDB");
+const Users = require('../models/userModel');
+
+
+var token;
+
+setupTestDB();
+
+describe("login", () => {
+	test("Return status 200 and data if email and password correct", async () => {
+		const res = await request(app).post("/api/login").send({
+			email: "thanhsang6325@gmail.com",
+			password: "123456",
+		});
+
+		expect(res.status).toBe(200);
+		token = res.body.access_token;
+	});
+});
