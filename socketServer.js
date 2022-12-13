4c162d8494aeda9c648aaa4a5fe2ebc3b354ac4a
Nguyễn Thanh Sang <79785988+ntSang18@users.noreply.github.com>
2022-12-13 22:13:50 +0700

    Update socketServer.js

--- socketServer.js
+++ socketServer.js
@@ -143,39 +143,6 @@ const SocketServer = (socket) => {
 			});
 		}
 	});
-
-	// Call User
-	// socket.on('callUser', data => {
-	//     users = EditData(users, data.sender, data.recipient)
-
-	//     const client = users.find(user => user.id === data.recipient)
-
-	//     if(client){
-	//         if(client.call){
-	//             socket.emit('userBusy', data)
-	//             users = EditData(users, data.sender, null)
-	//         }else{
-	//             users = EditData(users, data.recipient, data.sender)
-	//             socket.to(`${client.socketId}`).emit('callUserToClient', data)
-	//         }
-	//     }
-	// })
-
-	// socket.on('endCall', data => {
-	//     const client = users.find(user => user.id === data.sender)
-
-	//     if(client){
-	//         socket.to(`${client.socketId}`).emit('endCallToClient', data)
-	//         users = EditData(users, client.id, null)
-
-	//         if(client.call){
-	//             const clientCall = users.find(user => user.id === client.call)
-	//             clientCall && socket.to(`${clientCall.socketId}`).emit('endCallToClient', data)
-
-	//             users = EditData(users, client.call, null)
-	//         }
-	//     }
-	// })
 };
 
 module.exports = SocketServer;
